05b9e58d2fbf9e740d5ed0928dad3f73bc17c09f
julia-pizete <pizetejulia@gmail.com>
2026-02-24 11:38:41 -0300

    24/02

---
+++ aula01/script.js
@@ -0,0 +1,19 @@
+var nome = "Julia"
+
+var idade = 16;
+
+var altura = 1.59;
+
+console.log(nome);
+console.log(idade);
+console.log(altura);
+
+document.write("<h1> Olá, seja bem vindo ao Java Script</h1>")
+
+document.write(nome + "<br>");
+document.write(idade + "<br>");
+document.write(altura + "<br>");
+
+var dias = idade * 365;
+
+document.write("<p style='color : red'>Você já viveu" + dias +"dias</p>")
