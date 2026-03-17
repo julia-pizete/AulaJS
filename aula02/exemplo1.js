e86c94a797fbab8ca64108f780f78bba07997541
julia-pizete <pizetejulia@gmail.com>
2026-03-17 11:40:42 -0300

    17/3

---
+++ aula02/exemplo1.js
@@ -0,0 +1,8 @@
+function somar2numeros()
+{
+    let numero1, numero2, soma;
+
+    numero1=document.getElementById("numero1").Value;
+    numero2=document.getElementById("numero2").Value;
+    document.getElementById("resultado").innerHTML = "Olá !!!";
+}
