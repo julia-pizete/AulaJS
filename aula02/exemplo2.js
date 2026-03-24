743a9d6764356c211146906737024e3c4c0c4251
julia-pizete <pizetejulia@gmail.com>
2026-03-24 11:21:22 -0300

    24/03

---
+++ aula02/exemplo2.js
@@ -0,0 +1,13 @@
+function Exemplo2()
+
+{
+    let numero= Number( document.getElementById ("numero").value);
+
+    let quadrado, cubo;
+    quadrado = numero * numero;
+    cubo = numero * numero * numero;
+
+    document.getElementById("julia").innerHTML=
+     "<p> O quadrado do número é" + quadrado +  " e o cubo é " + cubo + "</p>";
+
+}
