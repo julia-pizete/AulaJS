a551c5e5f74fda1a6469cd9abea73ffee87d8c55
julia-pizete <pizetejulia@gmail.com>
2026-04-14 11:12:49 -0300

    144

---
+++ exercicio.js
@@ -0,0 +1,10 @@
+document.write("<h2>Calcular corrida</h2>");
+
+function Calcular()
+{
+    let numero1, numero2, soma;
+
+    numero1=document.getElementById("numero1").Value;
+    numero2=document.getElementById("numero2").Value;
+    document.getElementById("resultado").innerHTML = "Olá !!!";
+}
